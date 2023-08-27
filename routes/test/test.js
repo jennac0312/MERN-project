1741345469843bdaf844e04262e81ec7802bc441
Jenna C <jennac0312@gmail.com>
2023-08-27 19:00:03 -0400

    changed test.js controller file to index.js to see if i could omit the file name in it's import in routes test.js. i never doubted you dylan i am simply a skeptic at heart :P

--- routes/test/test.js
+++ routes/test/test.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const testCtrl = require("../../controllers/test/test")
+const testCtrl = require("../../controllers/test")
 
 router.get( '/', testCtrl.test )
 
