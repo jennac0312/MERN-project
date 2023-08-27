8cadad82e043e4ba47548e6fc74abe2204427e10
Jenna C <jennac0312@gmail.com>
2023-08-27 18:19:11 -0400

    skelly built. time to test proxy with api request from f to b

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <ol>
+        <li> authentication </li>
+        <li> back end auth route </li>
+        <li> front end form  </li>
+      </ol>
     </div>
   );
 }
